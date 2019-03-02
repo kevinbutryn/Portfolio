361c843458ceb59f89c4834e7e1c865d1e371288
Kevin Butryn <kevinbutryn@gmail.com>
2019-03-02 00:43:06 -0500

    finished

--- javascript/P5/Project Snake/sketch.js
+++ javascript/P5/Project Snake/sketch.js
@@ -1,66 +1,78 @@
 var rows, cols;
 let scale = 20;
-let yoff = 0.0;
-let roughness = .1;
-let low = 0;
-let high = 300;
+var snake;
+var direction;
+var score = 1;
+var reset_button;
 
 function setup() {
-  createCanvas(800,1000,WEBGL);
+  createCanvas(400,400);
   
+  reset_button = createButton('RESET GAME');
+  reset_button.mousePressed(resetGame);
   rows = width / scale;
   cols = height / scale;
 
-  r1 = 50;
-  g1 = 120;
-  b1 = 50;
-
-  r2 = 80;
-  g2 = 60;
-  b2 = 60;
-
-  c2 = color(r1,g1,b1);
-  c1 = color(r2,g2,b2);
-  
-
+  snake = new snake();
+  direction = createVector(0,1);
 }
 
 
 function draw() {
-  background(51);
-
-
-  translate(-height/2, 200 , height/2 - 200);
-  rotateX(PI + PI/3);
-  
-
+  frameRate(5);
+  background(150);
   strokeWeight(1);
-  
+  noFill();
   stroke(8);
-  // fill(255,0,0);
-
-
-
+  
   for (var y = 0; y <= rows; y++){
 
-    beginShape(TRIANGLE_STRIP);
+    beginShape(QUAD_STRIP);
 
     for (var x = 0; x <= cols; x++){
       x1 = x * scale;
       x2 = x * scale;
       y1 = y * scale;
       y2 = (y+1) * scale;
-      z1 = map( noise(x * roughness, y * roughness + yoff     ) , 0 , 1 , low , high )
-      z2 = map( noise(x * roughness, (y+1) * roughness  + yoff) , 0 , 1 , low , high )
-
-      let color = lerpColor(c1,c2,map(z1, low, high, 0, 1))
-      fill(color);
-      vertex(x1, y1, z1);
-      vertex(x2, y2, z2);
+  
+      vertex(x1, y1);
+      vertex(x2, y2);
     }
 
     endShape();
   }
+  
+  snake.update();
+  snake.show();
+}
 
-   yoff += .01; //speed at which the ground moves
+function keyPressed() {
+
+	if (keyCode === RIGHT_ARROW){
+		direction = createVector(1,0);
+	}else if (keyCode === LEFT_ARROW){
+		direction = createVector(-1,0);
+	}else if (keyCode === UP_ARROW){
+		direction = createVector(0,-1);
+	}else if (keyCode === DOWN_ARROW){
+		direction = createVector(0,1);
+  }
+  
+  snake.setDirection(direction);
+}
+
+function gameOver() {
+  // noStroke();      
+  textSize(80);
+  fill(240,25,10); 
+
+  textAlign(CENTER,CENTER);
+  text('THE END',0,height*.4,width);
+  text('SCORE: ' + score,0,height*.6,width);
+  noLoop();
+}
+
+function resetGame(){
+  snake.reset();
+  loop();
 }
