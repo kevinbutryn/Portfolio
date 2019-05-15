a412edc5c3b4960ed753353b326f41f90452357e
Kevin Butryn <kevinbutryn@gmail.com>
2019-05-14 22:18:07 -0400

    update name

--- javascript/P5/Project RayCasting Vehicle/particle.js
+++ javascript/P5/Project RayCasting Vehicle/particle.js
@@ -6,11 +6,17 @@ class Particle{
 
   constructor() {
     this.pos = createVector(start_posx,start_posy);
+    this.vel = createVector();	
+    this.acc = createVector();
     this.w = 20;
+    this.maxSpeed = 3;
     this.rays = [];
     this.alive = true;
     this.createRays();
   }
+  applyForce(force){
+    this.acc.add(force);
+	}
 
   createRays(){
     this.rays = [];
@@ -32,16 +38,40 @@ class Particle{
       //console.log ("im dead");
       fill(255,0,0);
     }
-    ellipse()
-    ellipse(this.pos.x, this.pos.y, this.w, this.w)
+    push();
+      translate (this.pos.x, this.pos.y)
+      rotate(this.vel.heading());
+      rectMode(CENTER);
+      rect(0, 0, 20,4);
+      //ellipse(this.pos.x, this.pos.y, this.w, this.w)
+    pop();
   }
 
   move(newX,newY){
     //move
     //this.pos.x = mouseX;
     //this.pos.y = mouseY;
-    this.pos.x = newX;
-    this.pos.y = newY;
+    
+    //this.pos.x = newX;
+    //this.pos.y = newY;
+    let force = createVector(0,0);
+    let num = random(0,3)
+    if (num > 2){
+      force = createVector(0,-1);
+    }else if (num < 1){
+      force = createVector(1,0);
+    } else {
+      force = createVector(-1,0);
+    }
+    //force = createVector(random(-.1,.1),random(-.1,.1))
+    
+
+    this.applyForce(force );
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.vel.limit(this.maxSpeed);
   }
 
   update(walls){
