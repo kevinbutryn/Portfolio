6af58fb7c4f17f1cf9e1bc57269edb98eedca3f2
kevin-PC\kevin <kevinbutryn@gmail.com>
2019-06-03 22:22:29 -0400

    insert example

---
+++ powershell/Coding Projects/insertExample.ps1
@@ -0,0 +1,40 @@
+﻿clear
+$server = "KEVIN-PC\SQLEXPRESS"
+$Database = "db1"
+
+$Connection = New-Object System.Data.SQLClient.SQLConnection
+$Connection.ConnectionString = "server='$Server';database='$Database';trusted_connection=true;"
+$Connection.Open()
+$Command = New-Object System.Data.SQLClient.SQLCommand
+$Command.Connection = $Connection
+#foreach($i in $list) {
+#}
+
+
+$sql = "INSERT INTO [dbo].[t1]
+           ([number])
+     VALUES
+           (1)"
+
+
+$Command.CommandText = $sql
+$Command.ExecuteReader()
+
+$Connection.Close()
+
+
+
+
+
+
+
+
+
+
+
+#    $sql ="if not exists (select 1 from [table_nm] where column_nm = '$i' ) 
+#        begin 
+#        insert table_nm
+#        select '$i'
+#        end
+#    " 
