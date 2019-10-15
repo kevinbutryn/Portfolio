da39d2578446f7676cc5b10e194672001f02ca2d
kevin-PC\kevin <kevinbutryn@gmail.com>
2019-10-14 23:20:00 -0400

    ran model

--- python/Ultimate_TicTacToe/ticTacToe.py
+++ python/Ultimate_TicTacToe/ticTacToe.py
@@ -7,39 +7,96 @@ BOARD_COLS = 3
 
 class State:
     def __init__(self, p1, p2):
-        self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
+        self.board = self.createBoard()
         self.p1 = p1
         self.p2 = p2
         self.isEnd = False
         self.boardHash = None
         # init p1 plays first
         self.playerSymbol = 1
+        self.score = np.full((3, 3), None) 
+        self.lastPosition = (0,0,0,0) 
+        self.p1Wins = 0
+        self.p2Wins = 0    
+        self.pTies = 0   
 
     # get unique hash of current board state
     def getHash(self):
-        self.boardHash = str(self.board.reshape(BOARD_COLS * BOARD_ROWS))
+        self.boardHash = str(self.board.reshape(BOARD_COLS * BOARD_ROWS * BOARD_COLS * BOARD_ROWS))
         return self.boardHash
 
-    def winner(self):
+    def createBoard(self):    
+        board = np.zeros((BOARD_ROWS, BOARD_COLS,BOARD_ROWS, BOARD_COLS))               
+        return board
+
+    def winnerBoard(self,lastPosition): #3x3 grid? 
         # row
+        # board = 3 X 3
+        lastx = lastPosition[0]
+        lasty = lastPosition[1]
+        board = self.board[lastx,lasty]
         for i in range(BOARD_ROWS):
-            if sum(self.board[i, :]) == 3:
+            if sum(board[i, :]) == 3:
+                self.score[lastx,lasty] = 1
+                # self.isEnd = True
+                return 1
+            if sum(board[i, :]) == -3:
+                self.score[lastx,lasty] = -1
+                # self.isEnd = True
+                return -1
+        # col
+        for i in range(BOARD_COLS):
+            if sum(board[:, i]) == 3:
+                self.score[lastx,lasty] = 1 
+                # self.isEnd = True
+                return 1
+            if sum(board[:, i]) == -3:
+                self.score[lastx,lasty] = -1
+                # self.isEnd = True
+                return -1
+        # diagonal
+        diag_sum1 = sum([board[i, i] for i in range(BOARD_COLS)])
+        diag_sum2 = sum([board[i, BOARD_COLS - i - 1] for i in range(BOARD_COLS)])
+        diag_sum = max(abs(diag_sum1), abs(diag_sum2))
+        if diag_sum == 3:
+            # self.isEnd = True
+            if diag_sum1 == 3 or diag_sum2 == 3:
+                self.score[lastx,lasty] = 1
+                return 1
+            else:
+                self.score[lastx,lasty] = -1
+                return -1
+
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if board[i, j] == 0:
+                    return None
+        
+        self.score[lastx,lasty] = 0
+        return 0
+
+    def winner(self): #whole game
+        
+        
+        # row 
+        for i in range(BOARD_ROWS):
+            if sum(filter(None, self.score[i, :])) == 3:
                 self.isEnd = True
                 return 1
-            if sum(self.board[i, :]) == -3:
+            if sum(filter(None, self.score[i, :])) == -3:
                 self.isEnd = True
                 return -1
         # col
         for i in range(BOARD_COLS):
-            if sum(self.board[:, i]) == 3:
+            if sum(filter(None, self.score[:, i])) == 3:
                 self.isEnd = True
                 return 1
-            if sum(self.board[:, i]) == -3:
+            if sum(filter(None, self.score[:, i])) == -3:
                 self.isEnd = True
                 return -1
         # diagonal
-        diag_sum1 = sum([self.board[i, i] for i in range(BOARD_COLS)])
-        diag_sum2 = sum([self.board[i, BOARD_COLS - i - 1] for i in range(BOARD_COLS)])
+        diag_sum1 = sum( filter(None, [self.score[i, i] for i in range(BOARD_COLS)]))
+        diag_sum2 = sum( filter(None, [self.score[i, BOARD_COLS - i - 1] for i in range(BOARD_COLS)]))
         diag_sum = max(abs(diag_sum1), abs(diag_sum2))
         if diag_sum == 3:
             self.isEnd = True
@@ -48,27 +105,50 @@ class State:
             else:
                 return -1
 
-        # tie
-        # no available positions
-        if len(self.availablePositions()) == 0:
-            self.isEnd = True
-            return 0
-        # not end
-        self.isEnd = False
-        return None
-
-    def availablePositions(self):
-        positions = []
+        # not end, spots available
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
-                if self.board[i, j] == 0:
-                    positions.append((i, j))  # need to be tuple
-        return positions
+                if self.score[i, j] == None:                      
+                    self.isEnd = False
+                    return None
+        
+        # tie
+        # no available positions
+        self.isEnd = True        
+        return 0
+
+    def availablePositions(self, lastPosition): # other part if flag = false or if in score array       
+        positions = []
+        flag = False
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if self.board[lastPosition[2],lastPosition[3],i, j] == 0:
+                    positions.append((lastPosition[2],lastPosition[3],i, j)) 
+                    flag = True
+        
+        if not flag:
+            for i in range(BOARD_ROWS):
+                for j in range(BOARD_COLS):
+                    if self.score[i][j] == None:
+                        for k in range(BOARD_ROWS):
+                            for l in range(BOARD_COLS):
+                                if self.board[i][j][k][l] == 0:
+                                    positions.append((i,j,k,l))
+                        
+
+
+
+        return (flag, positions)
 
     def updateState(self, position):
         self.board[position] = self.playerSymbol
+        self.winnerBoard(position)
+        self.lastPosition = position
         # switch to another player
         self.playerSymbol = -1 if self.playerSymbol == 1 else 1
+
+        # self.showBoard()
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
     # only when game ends
     def giveReward(self):
@@ -77,27 +157,36 @@ class State:
         if result == 1:
             self.p1.feedReward(1)
             self.p2.feedReward(0)
+            # print("p1 won")
+            self.p1Wins += 1
         elif result == -1:
             self.p1.feedReward(0)
             self.p2.feedReward(1)
-        else:
+            self.p2Wins += 1
+        else: # tie
             self.p1.feedReward(0.1)
             self.p2.feedReward(0.5)
+            self.pTies += 1
 
     # board reset
     def reset(self):
-        self.board = np.zeros((BOARD_ROWS, BOARD_COLS))
+        self.board = self.createBoard()
         self.boardHash = None
         self.isEnd = False
         self.playerSymbol = 1
+        self.score = np.full((3, 3), None)
+        self.lastPosition = (0,0,0,0)
 
     def play(self, rounds=100):
         for i in range(rounds):
             if i % 1000 == 0:
                 print("Rounds {}".format(i))
-            while not self.isEnd:
-                # Player 1
-                positions = self.availablePositions()
+                print ("p1 wins"  + str(self.p1Wins)) 
+                print ("p2 wins"  + str(self.p2Wins))
+                print ("ties wins" + str(self.pTies))
+            while not self.isEnd:                
+                # Player 1                
+                positions = self.availablePositions(self.lastPosition)[1] 
                 p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
                 # take action and upate board state
                 self.updateState(p1_action)
@@ -109,15 +198,17 @@ class State:
                 if win is not None:
                     # self.showBoard()
                     # ended with p1 either win or draw
+                    
                     self.giveReward()
                     self.p1.reset()
                     self.p2.reset()
-                    self.reset()
+                    self.reset()  
+                    # print("****************************************************************************")                  
                     break
 
                 else:
                     # Player 2
-                    positions = self.availablePositions()
+                    positions = self.availablePositions(self.lastPosition)[1]
                     p2_action = self.p2.chooseAction(positions, self.board, self.playerSymbol)
                     self.updateState(p2_action)
                     board_hash = self.getHash()
@@ -126,18 +217,20 @@ class State:
                     win = self.winner()
                     if win is not None:
                         # self.showBoard()
-                        # ended with p2 either win or draw
+                        # ended with p2 either win or draw                                          
+                        
                         self.giveReward()
                         self.p1.reset()
                         self.p2.reset()
                         self.reset()
+                        # print("****************************************************************************")
                         break
 
     # play with human
     def play2(self):
         while not self.isEnd:
             # Player 1
-            positions = self.availablePositions()
+            positions = self.availablePositions()[1]
             p1_action = self.p1.chooseAction(positions, self.board, self.playerSymbol)
             # take action and upate board state
             self.updateState(p1_action)
@@ -154,7 +247,7 @@ class State:
 
             else:
                 # Player 2
-                positions = self.availablePositions()
+                positions = self.availablePositions()[1]
                 p2_action = self.p2.chooseAction(positions)
 
                 self.updateState(p2_action)
@@ -170,20 +263,30 @@ class State:
 
     def showBoard(self):
         # p1: x  p2: o
-        for i in range(0, BOARD_ROWS):
-            print('-------------')
-            out = '| '
-            for j in range(0, BOARD_COLS):
-                if self.board[i, j] == 1:
-                    token = 'x'
-                if self.board[i, j] == -1:
-                    token = 'o'
-                if self.board[i, j] == 0:
-                    token = ' '
-                out += token + ' | '
-            print(out)
-        print('-------------')
-
+        for i in range(0, BOARD_ROWS):            
+                for k in range(0, BOARD_ROWS):                    
+                    out = '  '
+                    for j in range(0, BOARD_COLS):
+                        for l in range(0, BOARD_COLS):                            
+                            if self.board[i, j, k, l] == 1:
+                                token = 'x'
+                            if self.board[i, j, k, l] == -1:
+                                token = 'o'
+                            if self.board[i, j, k, l] == 0:
+                                token = ' '
+                            out += token + " "
+                            if l != BOARD_ROWS - 1 :
+                                out += '| '
+                        if j != BOARD_ROWS - 1:
+                            out += '  #   '   
+                    print(out)
+                    if k != BOARD_ROWS - 1:
+                        print(' -----------     -----------     -----------')
+                if i != BOARD_ROWS - 1:
+                    print()
+                    print('#  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #')
+                    print()
+        print('------------------------------------------')
 
 class Player:
     def __init__(self, name, exp_rate=0.3):
@@ -195,7 +298,7 @@ class Player:
         self.states_value = {}  # state -> value
 
     def getHash(self, board):
-        boardHash = str(board.reshape(BOARD_COLS * BOARD_ROWS))
+        boardHash = str(board.reshape(BOARD_COLS * BOARD_ROWS * BOARD_COLS * BOARD_ROWS))
         return boardHash
 
     def chooseAction(self, positions, current_board, symbol):
@@ -204,7 +307,7 @@ class Player:
             idx = np.random.choice(len(positions))
             action = positions[idx]
         else:
-            value_max = -999
+            value_max = -999            
             for p in positions:
                 next_board = current_board.copy()
                 next_board[p] = symbol
@@ -269,18 +372,51 @@ class HumanPlayer:
 
 if __name__ == "__main__":
     # training
-    # p1 = Player("p1")
-    # p2 = Player("p2")
-
-    # st = State(p1, p2)
-    # print("training...")
-    # st.play(50000)
-
-    # play with human
-    p1 = Player("computer", exp_rate=0)
-    p1.loadPolicy("policy_p1")
-
-    p2 = HumanPlayer("human")
+    p1 = Player("p1")
+    p2 = Player("p2")
 
     st = State(p1, p2)
-    st.play2()
+    print("training...")
+    st.play(80000)
+
+    p1.savePolicy()
+    p2.savePolicy()
+    # play with human
+
+
+    # p1 = Player("computer", exp_rate=0)
+    # p1.loadPolicy("policy_p1")
+
+    # p2 = HumanPlayer("human")
+
+    # st = State(p1, p2)
+    # st.play2()
+
+
+    #testing
+    # st.createBoard()
+
+    
+
+
+    #TODO
+    # available postions 
+    # winner
+    # play 
+    # feed reward
+    # choose action
+    # human play interation
+    # hard coded start game
+
+
+
+#Garbage 
+
+
+        
+        # print(board)   
+        # print("----")
+        # board[0][0][0][0] = "1"
+        # self.board[1][0][1][1] = "1"
+        # print(board)    
+    
