d92f38c5ca773315d07064d6a3daf6e108ca328c
kevinbutryn <kevinbutryn@gmail.com>
2020-06-22 00:21:11 -0400

    python code

--- python/Automated Trading/main.py
+++ python/Automated Trading/main.py
@@ -1,40 +1,30 @@
-import requests 
+import requests
 
-# API-url-prod = "https://api.tradestation.com/v2"
-# API-url-test = "https://sim-api.tradestation.com/v2"
-# APIKey = ""
-# APISecret = ""
-# API-url-Authorize = "https://api.tradestation.com/v2/authorize"
-
-
-print("hello")
+apikey = "brk4o8vrh5r9g3otirag"
 
 
 
+def API_quote(symbol):
+    r = requests.get('https://finnhub.io/api/v1/quote?symbol=' + symbol + '&token=' + apikey)
+    return r.json()
 
-# curl "https://api.tradestation.com/v2/data/symbol/AMZN?access_token=1JUjFiNFFGWEdDM"
+# o = Open price of the day
+# h = High price of the day
+# l = Low price of the day
+# c = Current price
+# pc = Previous close price
 
-# {
-# "Category": "string",
-# "Country": "US",
-# "Currency": "USD",
-# "Description": "string",
-# "DisplayType": 0,
-# "Error": "string",
-# "Exchange": "NYSE",
-# "ExchangeID": 0,
-# "ExpirationDate": "string",
-# "ExpirationType": "string",
-# "FutureType": "string",
-# "IndustryCode": "string",
-# "IndustryName": "string",
-# "LotSize": 0,
-# "MinMove": 0,
-# "Name": "string",
-# "OptionType": "string",
-# "PointValue": 0,
-# "Root": "string",
-# "SectorName": "string",
-# "StrikePrice": 0,
-# "Underlying": "string"
-# }
+company = "AAPL"
+
+json_info = API_quote(company)
+
+open_price_of_the_day = json_info['o']
+high_price_of_the_day = json_info['h']
+low_price_of_the_day = json_info['l']
+current_price = json_info['c']
+previous_close_price = json_info['pc']
+
+
+print(json_info)
+print(json_info['c'])
+
